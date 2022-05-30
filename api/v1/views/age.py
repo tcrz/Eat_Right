16b917a6fefd921bea3d67c261402b83d815da38
samie-ya <samrasolomon40@gmail.com>
2022-05-30 21:18:59 +0000

    Made Modifications to README

--- api/v1/views/age.py
+++ api/v1/views/age.py
@@ -6,13 +6,13 @@ from tables import storage
 from tables.age import Age
 
 
-@views.route('/age/<gender>/<activity>/<int:n>', methods=['GET'])
-def get_calorie_amount(gender, activity, n):
+@views.route('/age/<gender>/<activity>/<int:digit>', methods=['GET'])
+def get_calorie_amount(gender, activity, digit):
     """This function will retrieve exact calorie amount"""
-    if n > 100 or n < 4:
+    if digit > 100 or digit < 4:
         return make_response(jsonify({'error': 'Not With In Range'}), 404)
 
     for age in storage.all(Age).values():
         if age.gender == gender and age.activity_level == activity:
-            if age.min_age <= n and n <= age.max_age:
+            if age.min_age <= digit and digit <= age.max_age:
                 return age.make_dict()
